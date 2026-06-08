194e3ca36d9034614e5d5517abddf8214ef86f9e
corneliusfj16 <liusc1253@gmail.com>
2026-06-08 22:32:24 +0700

    CRUD User & CRUD Events
    Cornel

--- Frontend/app/src/main/java/com/mdp/caremate/data/repositories/AdminRepository.kt
+++ Frontend/app/src/main/java/com/mdp/caremate/data/repositories/AdminRepository.kt
@@ -1,11 +1,11 @@
 package com.mdp.caremate.data.repositories
 
-interface AdminRepository {
-    suspend fun getAll():List<User>
-    suspend fun getById(id:String):User?
-    suspend fun insert(content:String): Todo
-    suspend fun update(id:String,content:String,completed:Boolean): Todo
-    suspend fun delete(id:String): Todo
-
-    suspend fun  sync()
-}
+//interface AdminRepository {
+//    suspend fun getAll():List<User>
+//    suspend fun getById(id:String):User?
+//    suspend fun insert(content:String): Todo
+//    suspend fun update(id:String,content:String,completed:Boolean): Todo
+//    suspend fun delete(id:String): Todo
+//
+//    suspend fun  sync()
+//}
